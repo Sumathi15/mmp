3d4ea5cd5da2bd5f1d124ab606867170243af367
SK_admin <SK_admin@LAPTOP-1L9Q5GFL>
2019-11-01 19:30:44 -0400

    modifying the View History

--- mmp-java/src/main/java/com/mmp/pages/ViewHistory.java
+++ mmp-java/src/main/java/com/mmp/pages/ViewHistory.java
@@ -3,5 +3,6 @@ package com.mmp.pages;
 import com.mmp.base.TestBaseClass;
 
 public class ViewHistory extends TestBaseClass{
+	//change for git
 
 }
--- mmp-java/src/main/java/com/mmp/pages/ViewReports.java
+++ mmp-java/src/main/java/com/mmp/pages/ViewReports.java
@@ -3,5 +3,8 @@ package com.mmp.pages;
 import com.mmp.base.TestBaseClass;
 
 public class ViewReports extends TestBaseClass{
+	
+	//public ViewReports();
+	
 
 }
